0f1e49e8af9d4ce907ff7c29f6f1c830c80ded96
DESKTOP-G08HS3B\Lee Yi <leeyi45@gmail.com>
2026-08-05 21:55:22 +0800

    Add tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -15,8 +15,38 @@ function monarchGrammarTests(lang: ILanguageDefinition) {
         expect(grammar.keywords).not.toContain(each);
       }
     });
+
+    const illegalOperators: string[] | undefined = grammar.illegalOperators;
+    test.skipIf(illegalOperators === undefined)('If it has illegal operators, they should not also be in the operators list', () => {
+      for (const each of illegalOperators!) {
+        expect(grammar.operators).not.toContain(each);
+      }
+    });
   });
 }
+
+test('Every language should have a unique id', () => {
+  const idMap = Object.values(languages).reduce<Record<string, ILanguageDefinition[]>>((res, lang) => {
+    if (lang.id in res) {
+      return {
+        ...res,
+        [lang.id]: [...res[lang.id], lang]
+      };
+    }
+
+    return {
+      ...res,
+      [lang.id]: [lang]
+    };
+  }, {});
+
+  Object.entries(idMap).forEach(([id, langs]) => {
+    if (langs.length === 1) return;
+
+    const langStr = langs.map(({ name }) => name).join(', ');
+    expect.fail(`There are ${langs.length} languages with id ${id}: ${langStr}`);
+  });
+});
 
 describe.each(Object.values(languages))('$name', lang => {
   monarchGrammarTests(lang);
